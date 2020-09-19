c0262b8fd8c342d62baaafbd6c2d744dece3365c
Mert <ocalmerd@gmail.com>
2020-09-19 14:46:47 +0300

    Update11

--- SOFTWARE/Core/Src/Flash_RW.c
+++ SOFTWARE/Core/Src/Flash_RW.c
@@ -9,6 +9,8 @@
 
 
 void    FLASH_PageErase(uint32_t PageAddress);
+HAL_StatusTypeDef  HAL_FLASHEx_Erase(FLASH_EraseInitTypeDef *pEraseInit, uint32_t *PageError);
+
 
 uint32_t PageError;
 
@@ -23,21 +25,45 @@ void Flash_ErasePage( uint32_t pageaddress )
 	HAL_FLASH_Lock();
 }
 
-void Flash_EEPROM_Data_Write( uint8_t page, uint32_t address, uint32_t index, void *writeBuffer )
+HAL_StatusTypeDef Flash_EEPROM_Data_Write( uint8_t page, uint32_t address, uint32_t index, void *writeBuffer )
 {
 	uint32_t flashAddress = address + (index*2);
 
-	//Unlock Flash
-	HAL_FLASH_Unlock();
+    HAL_StatusTypeDef status = HAL_FLASH_Unlock();
+
+    /* Fill EraseInit structure*/
+    FLASH_EraseInitTypeDef EraseInitStruct;
+    EraseInitStruct.TypeErase = FLASH_TYPEERASE_PAGES;
+    EraseInitStruct.PageAddress = address;
+    EraseInitStruct.NbPages = page;
+    uint32_t error;
+
+
+    if (status != HAL_OK)
+        return status;
+
+    // perform the erase first
+    HAL_FLASHEx_Erase(&EraseInitStruct, &error);
+
+    if (error) { return -1; }
 
 	//Write to Flash
 	for( uint32_t i = 0; i < 1; i++ )
 	{
-		HAL_FLASH_Program( FLASH_TYPEPROGRAM_HALFWORD, flashAddress, ( (uint16_t *)writeBuffer)[i] );
+		status = HAL_FLASH_Program( FLASH_TYPEPROGRAM_HALFWORD, flashAddress, ( (uint16_t *)writeBuffer)[i] );
+		if (status != HAL_OK)
+			return status;
+
+
+	    HAL_FLASH_Unlock();
+
+	    return status;
 	}
 
 	//Lock the Flash space
 	HAL_FLASH_Lock();
+
+	return status;
 }
 
 void Flash_EEPROM_Data_Read( uint8_t page, uint32_t address, uint32_t index, void *readBuffer )
